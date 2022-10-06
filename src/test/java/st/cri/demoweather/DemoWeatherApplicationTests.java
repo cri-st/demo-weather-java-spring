d305796231891a0d05899f29d329d9a20521d38c
Crist <crist@Crists-MacBook-Pro.local>
2022-10-06 11:20:39 -0300

    Add Spring Boot's base project

---
+++ src/test/java/st/cri/demoweather/DemoWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package st.cri.demoweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
